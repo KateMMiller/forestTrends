5533ec34da9e7734375f74ada26b1f9ae67b0313
KateMMiller <kathryn_miller@nps.gov>
2021-09-22 14:05:15 -0400

    added facet option with group

--- R/plot_trend_response.R
+++ R/plot_trend_response.R
@@ -4,7 +4,9 @@
 #'
 #' @param df Data frame containing a column called Plot_Name, a column called cycle, and a column with at least one
 #' response variable. If Unit_Code is in the data frame, then a facet by park will be plotted.
-#' @param ylab Quoted title for y axis
+#' @param xlab Quoted title for x axis.
+#' @param ylab Quoted title for y axis.
+#' @param group Quoted column for facet wraps. If not specified, only 1 plot will be returned
 #'
 #' @import ggplot2
 #' @importFrom dplyr filter mutate
@@ -12,27 +14,55 @@
 #' @examples
 #' \dontrun{
 #'
+#' #----- Dataset with 1 park -----
 #' fake_df <- data.frame(Plot_Name = rep(paste0(rep("PARK.", 9), 1:9), each = 3),
 #'                       cycle = rep(1:3, times = 9),
 #'                       resp = runif(27, 0, 20))
 #'
 #' boot1 <- case_boot_lmer(fake_df, y = "resp", num_reps = 10, random_type = 'intercept', chatty = TRUE)
 #'
-#' plot_trend_response(boot1, "Tree Basal Area")
+#' plot_trend_response(boot1, xlab = 'cycle', ylab = 'BA') +
+#'   scale_x_continuous(breaks = c(1, 2, 3), labels = c("1", "2", "3"))
 #'
 #'
+#' #----- Dataset with 2 parks iterating through each park with purrr -----
+#' # Create fake dataset
+#' fake_2pk <- data.frame(Plot_Name = c(rep(paste0(rep("APRK-", 12), sprintf("%02d", 1:12)), each = 3),
+#'                        rep(paste0(rep("BPRK-", 12), sprintf("%02d", 13:24)), each = 3)),
+#'                        park = c(rep("APRK", 36), rep("BPRK", 36)),
+#'                        cycle = rep(1:3, times = 24),
+#'                        resp = runif(72, 0, 30))
+#'
+#' # Nest dataset by park
+#' nested_df <- fake_2pk %>% mutate(grp = park) %>% group_by(park) %>% nest()
+#'
+#' # Run case_boot_lmer on nested dataset
+#' boot2 <- nested_df %>% mutate(
+#'   model = map(data, ~case_boot_lmer(., x = "cycle", y = "resp", ID = "Plot_Name",
+#'                                     random_type = 'intercept', group = "grp",
+#'                                     num_reps = 100, chatty = TRUE)))
+#'
+#' # Compile results
+#' boot_results <- boot2 %>% select(park, model) %>% unnest(model) %>% select(-num_boots)
+#'
+#' # Plot results
+#' plot_trend_response(boot_results, xlab = "Cycle", ylab = "BA", group = "park") +
+#'   scale_x_continuous(breaks = c(1, 2, 3), labels = c("1", "2", "3"))
+
 #' }
 #'
 #' @export
 
-plot_trend_response <- function(df, ylab){
+plot_trend_response <- function(df, xlab, ylab, group = NA){
 
   df2 <- df %>%
            mutate(sign = ifelse(term == "Slope" & (lower95 > 0 | upper95 < 0), "sign", "nonsign")) %>%
-           filter(!term %in% c("Intercept", "Slope")) %>%
-           mutate(cycle = as.numeric(substr(term, 2, 2)))
+           filter(!term %in% c("Intercept", "Slope"))
 
-  p <- ggplot(df2, aes(x = cycle, y = estimate, shape = sign, linetype = sign))+
+  df2$time <- as.numeric(gsub("\\D", "", df2$term))
+
+  p <-
+    ggplot(df2, aes(x = time, y = estimate, shape = sign, linetype = sign))+
        geom_point()+
        geom_errorbar(aes(ymin = lower95, ymax = upper95),
                      width = 0.2, size = 0.5, linetype = 'solid', na.rm = TRUE)+
@@ -46,9 +76,9 @@ plot_trend_response <- function(df, ylab){
              panel.grid.minor = element_blank(),
              panel.border = element_rect(colour = "black", fill = NA, size = 0.1),
              legend.position = 'none')+
-       {if("Unit_Code" %in% names(df)) facet_wrap(~Unit_Code)}+
-       labs(x = "Cycle", y = ylab)+
-       scale_x_continuous(breaks = c(1,2,3), labels = c("1", "2", "3"))
+       {if(!is.na(group)) facet_wrap(~df2[[group]])}+
+       #scale_x_continuous(breaks = xbreaks, labels = xlabels)+
+       labs(x = xlab, y = ylab)
 
   return(p)
 }
