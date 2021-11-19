d2f7a5be2b1392b4d27b66555695d93f4daf8422
KateMMiller <kathryn_miller@nps.gov>
2021-11-19 14:21:08 -0500

    improved help doc

--- R/trend_lmer.R
+++ R/trend_lmer.R
@@ -1,19 +1,19 @@
-#' trend_lmer: fit random effects model with lmer and return results of interest
+#' trend_lmer: Fit random effects model with lmer and return results of interest
 #'
-#' @description fit random effects model in lmer, and return intercept, slope and predicted responses for each
+#' @description Fit random effects model in lmer, and return fixed effects intercept and slope and predicted responses for each
 #' time step (i.e. cycle or year) represented in the data. This is mostly an internal function run within case_boot_lmer(),
 #' but can be used as a stand alone function.
 #'
-#' @param df Data frame containing a column called Plot_Name, a column called cycle, and a column with at least one
-#' response variable.
+#' @param df Data frame containing an ID column that identifies each sample unit (e.g., Plot_Name), a column containing a time
+#' variable, and at least one column with a response variable.
 #' @param x Quoted time variable for trend analysis. Default is "cycle", but can also model by year. Must be numeric.
-#' @param y Quoted response variable in the data frame.
+#' @param y Quoted response variable in the data frame. Must be numeric
 #' @param ID Quoted name of column containing site or plot IDs. Default is "Plot_Name", and assumes the first 4 characters
 #' are a park code.
-#' @param random_type Specify intercept, slope, or custom. The intercept option (default) will fit a random intercept on plot with (1|Plot_Name) as
-#' random component. The slope option will fit a random slope model with (1 + cycle|Plot_Name)
-#' @param random_formula If random_type = "custom", must specify the random effects formula for the model in quotes. Otherwise leave blank.
-#' @param nest_var Quote column containing a grouping variable for nested random effects.
+#' @param random_type Specify "intercept", "slope", or "custom". The intercept option (default) will fit a random intercept model
+#' with (1|ID) as random component. The slope option will fit a random slope model with (1 + x|ID) as the random component.
+#' If "custom" is used, must also specify random_formula.
+#' @param random_formula If random_type = "custom", specify the random effects formula for the model in quotes. Otherwise leave blank.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom lme4 lmer
@@ -44,8 +44,7 @@
 #' @export
 
 trend_lmer <- function(df, x = "cycle", y, ID = "Plot_Name",
-                       random_type = c("intercept", "slope", "custom"), random_formula = NA,
-                       nest_var = NA){
+                       random_type = c("intercept", "slope", "custom"), random_formula = NA){
 
   if(is.null(df)){stop("Must specify df to run function")}
   if(is.null(x)){stop("Must specify x variable to run function")}
@@ -54,8 +53,6 @@ trend_lmer <- function(df, x = "cycle", y, ID = "Plot_Name",
   random_type <- match.arg(random_type)
   if(random_type == "custom" & is.na(random_formula)){stop("Must specify random formula of random_type = 'custom'")}
   stopifnot(c(x, y, ID) %in% names(df))
-  stopifnot(is.numeric(df[,x]))
-  stopifnot(is.numeric(df[,y]))
 
   # set up model
   mod_df <- data.frame(term = c("Intercept", "Slope"), estimate = NA_real_)
